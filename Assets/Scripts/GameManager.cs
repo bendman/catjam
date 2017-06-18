f25806ff892e0892e28e708d14998710264175db
bendman <duncan.ben@gmail.com>
2017-06-18 06:03:10 -0700

    scene placeholders and text serialization (for git)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public static void LoadStartScene()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	public static void LoadNextLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public static void LoadEndScene()
+	{
+		SceneManager.LoadScene(SceneManager.sceneCount - 1);
+	}
+
+	private void Awake()
+	{
+		
+	}
+	
+	private void Update()
+	{
+		
+	}
+}
